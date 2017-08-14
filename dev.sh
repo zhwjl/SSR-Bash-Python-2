0626c84ced69e40dbf7d2b4df92d7be21935a163
beststarry <1437617534@qq.com>
2017-08-14 21:31:26 +0800

    恢复更新

--- dev.sh
+++ dev.sh
@@ -20,7 +20,13 @@ while :; do echo
 done
 
 if [[ $devc == 1 ]];then
-	echo '更新被管理员停止了'
+	rm -rf /usr/local/bin/ssr
+	cd /usr/local/SSR-Bash-Python/
+	git pull
+	wget -N --no-check-certificate -O /usr/local/bin/ssr https://raw.githubusercontent.com/${GH_REPO}/master/ssr
+	chmod +x /usr/local/bin/ssr
+	echo 'SSR-Bash升级成功！'
+	ssr
 fi
 
 if [[ $devc == 2 ]];then
